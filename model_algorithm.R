e6c50c1288f91aaf951b0a0bbfff6af59fbd62a2
shuhaodexiaowu <zhangshuhaojs@163.com>
2020-08-27 19:21:44 +0800

    small correction

--- model_algorithm.R
+++ model_algorithm.R
@@ -23,6 +23,13 @@ unigramset<- readRDS("F:/coursera/datasciencecapstone/final/en_US/uni/uni_total.
 # I have remove the frequence of 1 Ngrams, still I find that the Ngrams with 2 frequence are alse meaningless
 # So i can cut this part to improve the effeciency.
 
+# Furthermore, I can calculate the coverage. Actually I can omit n<=5 rows for trigramset to achieve of coverage of 90% occurance
+# The other two set can also be filtered to achieve a smaller memory storage
+
+# trigramset <- filter(trigramset,n>3)
+# I didn't do it, because I use the Rprof function and find the time is about 0.5s It's quite acceptatble, so I retain these low
+# frequence grams so that I can have a higher accuracy.
+
 predictword<- function(sent){
        
         sent<-tolower(sent)
@@ -31,7 +38,10 @@ predictword<- function(sent){
         words <- unlist(str1)
         words <- words[!words %in% "the"]
         last_word <- words[length(words)]
-        last_2_word<- words[length(words)-1]
+        if (length(words)==1){
+                last_2_word<- "ABCD" # assign an character so that trigram cannot find ,than to bigram
+        }else{last_2_word<- words[length(words)-1]}
+        
         
         
         subset <- filter(trigramset,word1==last_2_word,word2==last_word)  ## find the possible trigram based on the two words
@@ -39,28 +49,24 @@ predictword<- function(sent){
                 subset2<-filter(subset,n==max(n))  #get the most frequant set
                 if (dim(subset2)[1]==1){
                         finalword<- subset2$word3
-                }
-                else{
+                }else{
                         # how to find the most possible word We can refer to the bigram or just compare the unigram freq
                         unisubset<- filter(unigramset,unigram %in% subset2$word3)
                         unisubset<- unisubset[order(-unisubset$n),] # order the tbl by n decresing
                         finalword<- unisubset$unigram[1]
                 }
-        }
-        else{
+        }else{
                 subset<-filter(bigramset,word1==last_word)
                 if(dim(subset)[1]>0){
-                        subset2<-filter(subset,n==manx(n))
+                        subset2<-filter(subset,n==max(n))
                         if(dim(subset2)[1]==1){
                                 finalword<- subset2$word2
-                        }
-                        else{
+                        }else{
                                 unisubset<- filter(unigramset,unigram %in% subset2$word2)
                                 unisubset<- unisubset[order(-unisubset$n),] # order the tbl by n decresing
                                 finalword<- unisubset$unigram[1]
                         }
-                }
-                else{
+                }else{
                         finalword<-"is" #when there is no match of ngrams(n=2/3), just pick a verb of the most freq
                 }
         }
@@ -68,3 +74,6 @@ predictword<- function(sent){
         
         
 }
+
+
+
